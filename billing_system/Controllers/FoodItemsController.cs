c1ba82f879925badcd38225fbf654a714c9be58e
codecraftwt <codecraftwt@gmail.com>
2024-08-14 15:17:21 +0530

    one more API added

--- billing_system/Controllers/FoodItemsController.cs
+++ billing_system/Controllers/FoodItemsController.cs
@@ -47,5 +47,39 @@ namespace billing_system.Controllers
                 };
             }
         }
+
+        [HttpGet("{id}")]
+        public async Task<ResponseModel> GetFoodItemsByFoodCategoryId(int id)
+        {
+            try
+            {
+                var resp = await _context.FoodItems.Where(x => x.FoodCategoryId == id).ToListAsync();
+                if (resp == null)
+                {
+                    return new ResponseModel()
+                    {
+                        Status = "Success",
+                        Message = $"Null Data found",
+                        Data = null
+                    };
+                }
+                return new ResponseModel()
+                {
+                    Status = "Success",
+                    Message = $"Data fetched Succesfully",
+                    Data = resp
+                };
+            }
+            catch (Exception ex) {
+                return new ResponseModel
+                {
+                    Status = "Failed",
+                    Message = $"Failed to fetch data: {ex.Message}",
+                    Data = null
+                };
+
+            }
+        }
+
     }
 }
